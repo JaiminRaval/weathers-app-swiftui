5b2cb1fc74f0599ca59b316cf64b99a65a86f287
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-07-16 15:21:07 +0530

    Initial Commit

---
+++ Weathers/WeathersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeathersApp.swift
+//  Weathers
+//
+//  Created by Jaimin Raval on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeathersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
